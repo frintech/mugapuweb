566fd466023ced6b749eec6a8b45793bacaf073b
frintech <query@frintech.com>
2015-09-20 18:05:37 +0530

    latest Source Code upload for Natchatra web

---
+++ NatchathraWeb/NatchathraWeb/Controllers/ReportController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NatchathraWeb.Models;
+namespace NatchathraWeb.Controllers
+{
+    public class ReportController : Controller
+    {
+        //
+        // GET: /Report/
+
+        public ActionResult PrintRepert()
+           {
+               return View();
+
+   
+          }
+
+    }
+}
---
+++ NatchathraWeb/NatchathraWeb/Models/ShoppingCartViewModel.cs
@@ -0,0 +1,22 @@
+﻿using NatchathraWeb.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NatchathraWeb.Models
+{
+    public class ShoppingCartViewModel
+    {
+        public List<ProductCart> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+    public class ShoppingCartRemoveViewModel
+    {
+        public string Message { get; set; }
+        public decimal CartTotal { get; set; }
+        public int CartCount { get; set; }
+        public int ItemCount { get; set; }
+        public int DeleteId { get; set; }
+    }
+}
